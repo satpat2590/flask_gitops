f36d7748bc76d3542984920e6c5db6b2fd8d3155
Satyam <patelsatyam100@gmail.com>
2023-09-15 19:33:43 -0400

    added tesseract OCR for pdf conversion

--- README.md
+++ README.md
@@ -20,8 +20,26 @@ Want to test it before?
 python test_app.py || python3 test_app.py
 ```
 
-## Github Actions Workflow
+## GitHub Actions Workflow
 
 The Github Actions workflow is interesting. It generates a job on a push or pull_request. 
 
 This job will run an ubuntu environment, and then use a pre-existing workflow to download a python (version 3.9) environment. Then, it will begin to download the dependencies and then run the test file to make sure it works properly.
+
+## Set Up for Embeddings
+
+If you made it here, it means you're probably trying to embed your own PDFs or other files so that the models can accurately reference them. Let's get you started so that you 
+can turn PDFs into well-formatted text. 
+
+# For Machines Using 'apt' Package Manager
+
+Go into your terminal and run the following: 
+
+```bash
+sudo apt-get update
+sudo apt-get install tesseract-ocr
+```
+
+This should install Google's Tesseract OCR (Optical Character Recognition) within your machine. With this installed, the python module (pytesseract) should be able to run 
+from flask_gitops/embeddings.py and convert PDFs -> Images -> Text 
+
--- embeddings.py
+++ embeddings.py
@@ -4,7 +4,42 @@ import tiktoken
 import PyPDF2 
 import torch 
 from transformers import BertTokenizer, BertModel
+from pdf2image import convert_from_path, convert_from_bytes
+from PIL import Image
+import tempfile
+from pytesseract import image_to_string
+import os  
 
+mpath = "learnings/neuronspaper.pdf"
+
+print(os.uname())
+
+### OPEN TEMP FILE (tempfile) -> TURN PDFs TO IMAGES (pdf2image) -> TURN IMAGES TO TEXT (pytesseract)
+texts = [] 
+
+def pdf_to_text(file_path):
+    extracted_text = ""
+    with tempfile.TemporaryDirectory() as path: 
+        images_from_path = convert_from_path(file_path, output_folder=path)
+        for image in images_from_path:
+            extracted_text += image_to_string(image)
+    texts.append(extracted_text)
+    print(extracted_text)
+
+def all_pdf_to_text():
+    file_list = [(x, y, z) for x, y, z in os.walk('./conversions')]
+    for idx, files in enumerate(file_list):
+        if idx < 10:
+            for file in files[2]:
+                pdf_to_text(os.path.join(files[0], file))
+        else:
+            break 
+
+all_pdf_to_text()
+
+print(len(texts))
+
+### INITIALIZE THE TIKTOKEN TOKEN ENCODER
 encoding = tiktoken.get_encoding("cl100k_base")
 
 ### ENCODING EXAMPLE
@@ -13,14 +48,16 @@ encoding = tiktoken.get_encoding("cl100k_base")
 #print([encoding.decode_single_token_bytes(x) for x in tokens])
 
 
-### READING PDF FROM THE ./learnings FOLDER
-reader = PyPDF2.PdfReader("learnings/Implementation_Consultant.pdf")
+### USING PyPDF2 TO OPEN PDFs FROM THE ./learnings FOLDER
+reader = PyPDF2.PdfReader(mpath)
 pdf_to_embeddings = ""
 
 for page in reader.pages:
     pdf_to_embeddings += page.extract_text()
 
-### TURNING STRINGS INTO TOKENS
+#print(pdf_to_embeddings)
+
+### TURNING STRINGS INTO TOKENS USING TIKTOKEN
 tokenize = encoding.encode(pdf_to_embeddings)
 decoded = [encoding.decode_single_token_bytes(x) for x in tokenize]
 
@@ -29,8 +66,9 @@ def get_embeddings(text, model="text-embedding-ada-002"):
     text = text.replace("\n", " ")
     return openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
 
+### INITIALIZE THE TIKTOKEN TOKEN ENCODER
 
-### BERT TOKENIZER & TORCH EMBEDDING PRACTICE
+### BERT TOKENIZER & EMBEDDDING + TORCH TENSOR PRACTICE
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 # Tokens created from using the 'tokenize' method, which simply creates the word-based tokens
 tokens = tokenizer.tokenize("This is an example of a tokenized text")
@@ -42,4 +80,4 @@ example_token_id = tokenizer.convert_tokens_to_ids(['example', 'testing'])
 example_embedding = model.embeddings.word_embeddings(torch.tensor(example_token_id))
 
 cos = torch.nn.CosineSimilarity(dim=1)
-print(example_embedding)
+#print(example_embedding)
